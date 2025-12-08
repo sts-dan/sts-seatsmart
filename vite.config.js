f57d5e51b77d13f7f2e12c6837a3ad7781a43920
sts-dan <danmalex@mit.edu>
2025-12-08 12:46:56 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@
 // Retrieved 2025-12-08, License - CC BY-SA 4.0
 
 import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react' // Make sure this is imported
 import postcssNesting from 'postcss-nesting';
 
 export default defineConfig({
+    plugins: [react()],
     css: {
         postcss: {
             plugins: [
